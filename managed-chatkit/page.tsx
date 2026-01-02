e4ceae3db0d59928ee5bac82797993e392e68e35
Velenje <107798142+Velenje@users.noreply.github.com>
2026-01-02 18:15:41 +0100

    Create page.tsx

---
+++ managed-chatkit/page.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <main>
+      <h1>OpenAI ChatKit Starter</h1>
+      <Link href="/chat">Go to Chat</Link>
+    </main>
+  );
+}
